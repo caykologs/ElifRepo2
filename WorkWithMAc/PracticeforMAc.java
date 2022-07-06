59f3cb96f13917242270166f1cd640634685ad4e
caykologs <akyelifus@gmail.com>
2022-07-05 19:25:42 -0500

    my first prac

---
+++ WorkWithMAc/PracticeforMAc.java
@@ -0,0 +1,7 @@
+package WorkWithMAc;
+
+public class PracticeforMAc {
+    public static void main(String[] args) {
+        System.out.println("This is for Mac");
+    }
+}
---
+++ WorkWithMAc/SecondPrac.java
@@ -0,0 +1,8 @@
+package WorkWithMAc;
+
+public class SecondPrac {
+
+    public static void main(String[] args) {
+        System.out.println("This is second prac");
+    }
+}
